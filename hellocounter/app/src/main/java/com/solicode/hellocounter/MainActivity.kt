82ace14349fd94c09ea7b52690016295f1cf706c
Yousra-akg <akagouyoussra@gmail.com>
2025-10-02 16:50:35 +0100

    ajout UI de base

--- hellocounter/app/src/main/java/com/solicode/hellocounter/MainActivity.kt
+++ hellocounter/app/src/main/java/com/solicode/hellocounter/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.solicode.hellocounter.ui.MainScreen
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -18,7 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { App() }
+        setContent { MaterialTheme { MainScreen() } }
     }
 }
 
@@ -45,6 +46,6 @@ fun Greeting() {
 }
 @Preview(showBackground = true)
 @Composable
-fun PreviewApp() {
-    App()
+fun PreviewMainScreen() {
+    MaterialTheme { MainScreen() }
 }
